52a70b4f095a3064d93e1ef79fd175c140ae6f0d
Bartosz_Kawa <bartekkawa2@gmail.com>
2025-03-09 22:16:05 +0100

    Collecting data works, and charts in frontend

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+import os
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # Define the path to the static folder.
+    static_path = os.path.join(app.root_path, 'static')
+
+    # List only CSV files.
+    csv_files = [f for f in os.listdir(static_path) if f.endswith('.csv')]
+
+    # Build a list of dictionaries containing file names and their URL paths.
+    file_list = [{"name": f, "url": url_for('static', filename=f)} for f in csv_files]
+
+    return render_template("home.html", csv_files=file_list)
 
 
-@app.route('/')
-def hello():
-    return render_template('home.html')
+if __name__ == "__main__":
+    app.run(debug=True)

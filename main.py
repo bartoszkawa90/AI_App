3c574df11733737eda53d75091ad43d10b084782
Bartosz_Kawa <bartekkawa2@gmail.com>
2025-03-13 17:25:18 +0100

    Some update to frontend and backend

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify, request
 import os
-from data_operations import download_stock_data, get_stock_data_from_csv
-from list_of_actions import stocks
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    # Define the path to the static folder.
-    static_path = os.path.join(app.root_path, 'static')
+    # Define the path to the 'stocks' folder within the static directory
+    stocks_path = os.path.join(app.root_path, 'static', 'stocks')
 
-    # List only CSV files.
-    csv_files = [f.split('.')[0] for f in os.listdir(static_path) if f.endswith('.csv')]
+    # List only CSV files in the stocks directory
+    csv_files = [f.split('.')[0] for f in os.listdir(stocks_path) if f.endswith('.csv')]
 
-    # Build a list of dictionaries containing file names and their URL paths.
-    file_list = [{"name": f, "url": url_for('static', filename=f)} for f in csv_files]
+    # Build a list of dictionaries containing file names and their URL paths
+    file_list = [
+        {"name": f, "url": url_for('static', filename=f'stocks/{f}.csv')}
+        for f in csv_files
+    ]
 
     return render_template("home.html", csv_files=file_list)
 
 @app.route('/update', methods=['POST'])
 def update():
-    """Collect new data from all considered stocks"""
+    pass
 
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
